67243df1f5b34228adab8d550b5aa0d0923d31e7
Devilak12 <mrakhi432@gmail.com>
2019-09-05 09:05:47 +0530

    project_started

---
+++ main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void seaech(){
+
+}
+void input()
+{
+
+}
+void output()
+{
+
+}
+void main(){
+    int a ;
+    char usn[50],pas[50];
+    clrscr();
+    printf("Let us start:\n enter a admin username");
+    scanf("%s",&usn);
+    printf("\nEnter password for account");
+    scanf("%s",&pas);
+    clrscr();
+    printf("\n Starting System\n");
+}
